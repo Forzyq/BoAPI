79a48c0d30f6e270c8d2c5a29c1a042ba9e13b4a
tangwenbo <f6883351@163.com>
2024-10-18 10:26:54 +0800

    初始化

---
+++ src/main/java/cdu/amber/boapibackend/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package cdu.amber.boapibackend.controller;
+
+import cdu.amber.boapibackend.common.BaseResponse;
+import cdu.amber.boapibackend.common.ErrorCode;
+import cdu.amber.boapibackend.common.ResultUtils;
+import cdu.amber.boapibackend.exception.BusinessException;
+import cdu.amber.boapibackend.model.dto.postthumb.PostThumbAddRequest;
+import cdu.amber.boapibackend.model.entity.User;
+import cdu.amber.boapibackend.service.PostThumbService;
+import cdu.amber.boapibackend.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
